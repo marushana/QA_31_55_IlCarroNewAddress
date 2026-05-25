503e026da0e3d6eda77daa2546f63e3840c5d573
marushana <marushana@yandex.ru>
2026-05-25 14:14:34 +0300

    Create new structure

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,27 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+    WebDriver wd;
+    HelperUser helperUser;
+
+    public void init(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wd.navigate().to("https://icarro-v1.netlify.app/search?page=0&size=10");
+        helperUser = new HelperUser(wd);
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
+}
---
+++ src/main/java/manager/HelperBase.java
@@ -0,0 +1,55 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HelperBase {
+    WebDriver wd;
+
+    public HelperBase(WebDriver wd) {
+        this.wd = wd;
+    }
+
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        clearNew(element);
+        if (text!=null){
+            element.sendKeys(text);
+        }
+    }
+
+    public void clearNew(WebElement element){
+        String os = System.getProperty("os.name");
+        element.sendKeys(" ");
+        if (os.startsWith("Win")){
+            element.sendKeys(Keys.CONTROL, "a");
+        }else {
+            element.sendKeys(Keys.COMMAND, "a");
+        }
+        element.sendKeys(Keys.DELETE);
+    }
+
+    public void click(By locator){
+        WebElement element = wd.findElement(locator);
+        element.click();
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isElementPresent(By locator){
+        List<WebElement> list = wd.findElements(locator);
+        return list.size()>0;
+    }
+}
